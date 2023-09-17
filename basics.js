42d6ded5bcdce63b2723eb709e14b8071e52c716
Gourikrishnan <gouri7497@gmail.com>
2023-09-17 08:47:12 +0530

    first commit

---
+++ basics.js
@@ -0,0 +1,76 @@
+//function sum(x,y) {
+   //let z=x+y;
+   //console.log("sum="+z); 
+//}
+//sum(10,10);
+
+//function welcome(name)
+//{
+   // console.log("welcome "+name);
+
+//}
+//welcome("priya");
+
+// Addition Operator
+//let a = 1 + 2
+//console.log(a);
+  
+// Subtraction Operator
+///let b = 10 - 7
+//console.log(b);
+  
+// Multiplication Operator
+//let c = 3 * 3
+//console.log(c);
+  
+// Division Operator
+//let d = 1.0 / 2.0
+//console.log(d);
+  
+// Modulas Operator
+//let e = 9 % 5
+//console.log(e);
+
+// Increment Operator
+//let g = 2;
+//g1 = g++;
+//console.log(g);
+  
+// Decrement Operator
+//let h = 2;
+//h1 = h--;
+//console.log(h);
+
+// Assigning values
+//let val1 = 5;
+//let val2 = 5;
+
+// Equality Operator
+//let val1 = 5;
+    //let val2 = 5;
+//console.log(val1 == val2);
+
+ // Strict equality Operator
+ //let val1 = 5;
+   // let val2 = 5;
+ //console.log(val1 === val2);
+
+ // Greater than Operator
+ //let val1 = 5;
+   // let val2 = 5;
+ //console.log(val1 > val2);
+
+// Less than Operator
+//let val1 = 5;
+    //let val2 = 5;
+//console.log(val1 < val2);
+
+// Greater than or equal Operator
+//let val1 = 5;
+  //  let val2 = 5;
+//console.log(val1 >= val2);
+ 
+// Less than or equal Operator
+let val1 = 5;
+let val2 = 5;
+console.log(val1 <= val2);
---
+++ loop.js
@@ -0,0 +1,85 @@
+//print 1 to 5
+//console.log(1);
+//console.log(2);
+//console.log(3);
+//console.log(4);
+//console.log(5);
+
+//for loop
+//for(let i =1;i<=5;i++){
+   // console.log(i)
+//}
+
+//for(let i =0;i<5;i++){
+    //console.log(i+1)
+//}
+
+//print array elements
+//let fruits = ["Apple","Mango","Orange","Grapes"]
+//for(let i = 0;i<fruits.length;i++){
+    //console.log(fruits[i])
+//}
+
+//for(let i in fruits){
+    //console.log(fruits[i];)
+//}
+// object
+//let person ={
+    //name:"Tiya",
+    //age:10
+//}
+   //for(let i in person){
+//console.log(person[i])
+   //}
+
+   // of
+   //let fruits = ["Apple","Mango","Orange","Grapes"]
+   //for(let i of fruits){
+    //console.log(i)
+   //}
+
+   //print sting using of
+   //var myName = "Tiya";
+   //for(let i of myName){
+    //console.log(i)
+   //}
+
+   // using While Loop.................
+
+   //print 1 to 5
+   //let i = 1;
+   //while(i<=5){
+    //console.log(i)
+    //i++
+   //}
+
+   // array using while loop.........
+
+   //let fruits = ["Apple","Mango","Orange","Grapes"]
+   //let i=0;
+   //while(i<fruits.length){
+//console.log(fruits[i]);
+//i++
+  // }
+
+  //do while............
+  //prin5 1-5
+  //let i = 1
+  //do{
+    //console.log(i);
+    //i++
+  //}while(i<=5)
+
+  //array using do while........
+  //let fruits = ["Apple","Mango","Orange","Grapes"]
+  //let i=0;
+  //do {
+   // console.log(fruits[i])
+    //i++
+  //} while (i<fruits.length);
+
+  
+
+
+
+
